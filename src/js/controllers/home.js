dcffe84e164d41966225758737330d99009916ff
Joshua Bradford <jjyet@yahoo.com>
2017-03-17 13:31:38 -0400

    added working sign out button and auto send in the headers

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,21 +1,32 @@
 const SERVER_URL = 'https://mysterious-tundra-23151.herokuapp.com';
 
-function Home ($scope, $http, $state){
+function Home ($scope, $http, $state, $cookies, $rootScope){
 
   $scope.photos = [];
 
   function init () {
-    $http.get(`${SERVER_URL}/photos`).then(resp => {
-      console.log(resp);
-      $scope.photos = resp.data;
-      console.log($scope.photos)
-    });
+    if($rootScope.loggedIn){
+      $http.get(`${SERVER_URL}/photos`).then(resp => {
+        //console.log(resp);
+        $scope.photos = resp.data;
+        //console.log($scope.photos)
+      });
+    }else{
+      $state.go('signup');
+    }
   }
 
   init();
 
+  $scope.signOut = function () {
+    $rootScope.loggedIn = false;
+    $cookies.remove('access-token');
+    $http.defaults.headers.common['access-token'] = null;
+    $state.go('signup');
+  };
+
 }
 
-Home.$inject = ['$scope', '$http', '$state'];
+Home.$inject = ['$scope', '$http', '$state', '$cookies', '$rootScope'];
 
 export default Home;
--- src/js/controllers/login.js
+++ src/js/controllers/login.js
@@ -1,16 +1,28 @@
 const SERVER_URL = 'https://mysterious-tundra-23151.herokuapp.com';
 
-function Login ($scope, $http, $state, $cookies){
+function Login ($scope, $http, $state, $cookies, $rootScope){
+  $scope.notifications = [];
+
+  $scope.removeMsg = (msg) => {
+    var removed = $scope.notifications.filter(x => x != msg);
+    $scope.notifications = removed;
+  };
+
+
   $scope.login = function (data) {
     console.log($cookies);
     var url = `${SERVER_URL}/login`;
     $http.post(url, data).then(resp => {
-      $cookies.put('access-token', resp.data.token)
+      $cookies.put('access-token', resp.data.token);
+      $rootScope.loggedIn = true;
+      $http.defaults.headers.common['access-token'] = resp.data.token;
       $state.go('home');
+    }).catch(error => {
+      $scope.notifications.push(error.data.message);
     });
   };
 }
 
-Login.$inject = ['$scope', '$http', '$state', '$cookies'];
+Login.$inject = ['$scope', '$http', '$state', '$cookies', '$rootScope'];
 
 export default Login;
