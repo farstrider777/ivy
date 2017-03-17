43f7dc7304444878a0b7cae391954456ebc9a056
Joshua Bradford <jjyet@yahoo.com>
2017-03-17 18:45:44 -0400

    made it so logged in users go to home page first

--- src/js/controllers/signUp.js
+++ src/js/controllers/signUp.js
@@ -1,10 +1,16 @@
 const SERVER_URL = 'https://mysterious-tundra-23151.herokuapp.com';
 
-function SignUp ($scope, $http, $state){
-  // $scope.test2 = function(){
-  //   console.log('hi from signup')
-  // };
+function SignUp ($scope, $http, $state, $cookies){
 
+  var token = $cookies.get('access-token');
+
+  function init(){
+    if (token) {
+      $state.go('home');
+    }
+  }
+
+  init();
 
   $scope.enter = function (data) {
     var url = `${SERVER_URL}/users`;
@@ -16,6 +22,6 @@ function SignUp ($scope, $http, $state){
 
 }
 
-SignUp.$inject = ['$scope', '$http', '$state'];
+SignUp.$inject = ['$scope', '$http', '$state', '$cookies'];
 
 export default SignUp;
