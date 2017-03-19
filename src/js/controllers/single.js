12a6b3afed7ed36bb3f8c77cfe5eec82a921f94f
Joshua Bradford <jjyet@yahoo.com>
2017-03-19 09:34:23 -0400

    cleaned up more console logs, fixed wrong placeholders on signup page

--- src/js/controllers/single.js
+++ src/js/controllers/single.js
@@ -15,30 +15,17 @@ function Single ($scope, $http, $state, $cookies, $rootScope){
   }
 
   $http.get(`${SERVER_URL}/likes/${chosen}`).then(resp => {
-      $scope.likes = resp.data.likes;
-  })
+    $scope.likes = resp.data.likes;
+  }).catch(error => {
+    $scope.notifications.push(error.data.message);});
 
-  $scope.comments = []
+  $scope.comments = [];
 
   $http.get(`${SERVER_URL}/comments/${chosen}`).then(resp => {
-      $scope.comments = resp.data;
-      console.log(resp.data[0].userId);
-      console.log(resp)
-
-      // for(var count = 0; count < $scope.comments.length; count++ ){
-      //   var id = resp.data[count].userId;
-      //
-      //   $http.get(`${SERVER_URL}/users/name/${id}`).then(resp =>
-      //   $scope.comments[count].name = resp.data.username
-      //   //console.log($scope.comments.name)
-      //  );
-
-    //  }
-
-
+    $scope.comments = resp.data;
   })
-  // .catch(error => {
-  // $scope.notifications.push(error.data.message);
+  .catch(error => {
+    console.log(error.data.message);});
 
   init();
 
@@ -50,33 +37,24 @@ function Single ($scope, $http, $state, $cookies, $rootScope){
   };
 
   $scope.dolike = function(){
-    var numLikes = $scope.likes
-    numLikes++
-    var data = { likes: numLikes }
+    var numLikes = $scope.likes;
+    numLikes++;
+    var data = { likes: numLikes };
     $scope.likes++;
 
 
     var url = `${SERVER_URL}/likes/${chosen}`;
     $http.put(url, data).then(resp => {
-
-      // console.log(resp.data.text);
-      // $window.location.reload ();
-
     }).catch(error => {
       console.log(error);
     });
-  }
+  };
 
   $scope.enter = function (data) {
-
     var url = `${SERVER_URL}/comments/${chosen}`;
     $http.post(url, data).then(resp => {
-
       $scope.recent = resp.data.text;
       $scope.comment.text = '';
-
-    //  $window.location.reload();
-
     }).catch(error => {
       console.log(error);
     });
