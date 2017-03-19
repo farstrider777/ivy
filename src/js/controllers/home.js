73f5b447a3ac4b986ee02ea53f9dc47060b96dd1
Joshua Bradford <jjyet@yahoo.com>
2017-03-19 08:53:00 -0400

    added proper likes pluralization

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -5,22 +5,9 @@ function Home ($scope, $http, $state, $cookies, $rootScope){
   $scope.photos = [];
 
   function init () {
-
-    // $http.get(`${SERVER_URL}/photos/likes/all`).then(resp => {
-    //   //console.log(resp.data);
-    // })
-
-
     if($rootScope.loggedIn){
       $http.get(`${SERVER_URL}/photos`).then(resp => {
         $scope.photos = resp.data;
-         //console.log($index.photos);
-        // for(var count = 0; count < $scope.photos.length; count++){
-        //   $scope.photos[count].likes = count;
-
-          //console.log(likeArray[count]);
-      //  }
-
       });
     }else{
       $state.go('signup');
